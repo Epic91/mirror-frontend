e556ac476a5985a4174fb026bf8b179991c7e73a
Epic91 <hernandeze791@gmail.com>
2021-03-18 19:19:25 -0500

    Nav bar links working

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Nav = ()=> {
     return(
         <nav>
             <h3>Logo</h3>
             <ul className="nav-links">
-                <li>Home</li>
-                <li>Calendar</li>
-                <li>Entries</li>
-                <li>Highlights</li>
-                <li>New Entry</li>
-                <li>Sign Out</li>
+                <Link to="/">
+                    <li>Home</li>
+                </Link>
+                <Link to="/calendar">
+                    <li>Calendar</li>
+                </Link>
+                <Link to="/entries">
+                    <li>Entries</li>
+                </Link>
+                <Link to="/highlights">
+                    <li>Highlights</li>
+                </Link>
+                <Link to="entries">
+                    <li>New Entry</li>
+                </Link>
+                <Link to="/login">
+                    <li>Sign Out</li>
+                </Link>
             </ul>
         </nav>
     )
